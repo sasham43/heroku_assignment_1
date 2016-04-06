a6015e988afc5a07d6fe418d220591e534d745ec
sasham43 <sashasemail@gmail.com>
2016-04-06 11:29:23 -0500

    Change city to minneapolis, add fields for weather data

--- public/script.js
+++ public/script.js
@@ -3,7 +3,7 @@ $(function(){
 
   var apiKey = '945023da3da01614';
   var urlBase = 'https://api.wunderground.com/api/';
-  var urlAdditions = '/geolookup/conditions/q/IA/Cedar_Rapids.json'
+  var urlAdditions = '/geolookup/conditions/q/MN/Minneapolis.json'
   var fullUrl = urlBase + apiKey + urlAdditions;
 
   $.ajax({
@@ -12,6 +12,18 @@ $(function(){
     data: 'json'
   }).done(function(response){
     console.log(response);
+    var city = response.location.city;
+    var state = response.location.state;
+    var temp = response.current_observation.temp_f;
+
+    var iconUrl = response.current_observation.icon_url;
+    var iconElement = '<img src=\"' + iconUrl + '\"/>';
+
+    $("#city").append(city);
+    $("#state").append(state);
+    $("#temp").append(temp);
+    $("#icon").append(iconElement);
+
     // $("body").append()
   });
 })
