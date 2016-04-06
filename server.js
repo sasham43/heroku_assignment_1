d3ab105e4be6bf3a3586d305f406a69e916c06cd
sasham43 <sashasemail@gmail.com>
2016-04-06 11:07:26 -0500

    Hard mode, today date

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var app = express();
 
-
+var today = new Date();
+var todayDate = today.getDate();
+var todayString = 'Today is the ' + todayDate + 'th';
 
 app.get('/', function(req, res){
-  res.send('Hello world!');
+  res.send(todayString);
 });
-
-
 
 var server = app.listen(process.env.PORT || 3000, function(){
   var port = server.address().port;
