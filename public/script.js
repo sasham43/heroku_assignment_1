27635a45e8c6b6e42dd49b72b4439c04df22b35d
sasham43 <sashasemail@gmail.com>
2016-04-06 11:23:38 -0500

    Add weatherunderground API logic

--- public/script.js
+++ public/script.js
@@ -1,3 +1,17 @@
 $(function(){
-  
+  console.log("jquery loaded");
+
+  var apiKey = '945023da3da01614';
+  var urlBase = 'http://api.wunderground.com/api/';
+  var urlAdditions = '/geolookup/conditions/q/IA/Cedar_Rapids.json'
+  var fullUrl = urlBase + apiKey + urlAdditions;
+
+  $.ajax({
+    url: fullUrl,
+    type: 'GET',
+    data: 'json'
+  }).done(function(response){
+    console.log(response);
+    // $("body").append()
+  });
 })
